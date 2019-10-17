21cf3a03e671d46856cd4a7fe9120b8858333485
James Robinson <james.em.robinson@gmail.com>
2019-10-17 11:16:34 +0100

    Automatically extract management subscription id

--- secure_research_environment/initialise_terraform.py
+++ secure_research_environment/initialise_terraform.py
@@ -49,7 +49,8 @@ def get_storage_account_key(credentials, subscription_id, resource_group, storag
     return storage_account_key
 
 
-def write_terraform_config(storage_account_name, storage_container_name, storage_account_key, subscription_id):
+def write_terraform_config(storage_account_name, storage_container_name, storage_account_key,
+                           subscription_id, management_subscription_id):
     # Write Terraform configuration
     terraform_lines = [
         'terraform {',
@@ -64,6 +65,11 @@ def write_terraform_config(storage_account_name, storage_container_name, storage
         'provider "azurerm" {',
         '  version         = "=1.35"',
         '  subscription_id = "{}"'.format(subscription_id),
+        '}',
+        'provider "azurerm" {',
+        '  alias           = "shm_management"',
+        '  version         = "=1.35"',
+        '  subscription_id = "{}"'.format(management_subscription_id),
         '}',
         'provider "null" {',
         '  version = "=2.1"',
@@ -80,7 +86,7 @@ def write_terraform_config(storage_account_name, storage_container_name, storage
         for line in terraform_lines:
             f_config.write(line + "\n")
 
-def write_terraform_variables(config, tenant_id):
+def write_terraform_variables(config, tenant_id, management_subscription_id):
     def walk(node, pre=None):
         pre = pre[:] if pre else []
         if isinstance(node, dict):
@@ -97,6 +103,12 @@ def write_terraform_variables(config, tenant_id):
     terraform_lines = [
         'output "tenant_id" {',
         '    value = "{}"'.format(tenant_id),
+        '}',
+        'data "azurerm_subscriptions" "shm_management" {',
+        '  display_name_contains = "{}"'.format(config["shm"]["subscriptionName"]),
+        '}',
+        'output "shm_subscription_id" {',
+        '    value = "${data.azurerm_subscriptions.shm_management.subscriptions.0.subscription_id}"',
         '}',
     ]
 
@@ -146,6 +158,7 @@ def main():
     parser.add_argument("-g", "--resource-group", type=str, default="RG_TERRAFORM_BACKEND", help="Resource group where the Terraform backend will be stored")
     parser.add_argument("-c", "--config", type=str, default="dsg_9_full_config.json", help="Name of the configuration file to use")
     parser.add_argument("-a", "--azure-group-id", type=str, default="347c68cb-261f-4a3e-ac3e-6af860b5fec9", help="ID of an Azure group which contains all project developers. Default is Turing's 'Safe Haven Test Admins' group.")
+    parser.add_argument("-t", "--tenant", type=str, default="4395f4a7-e455-4f95-8a9f-1fbaef6384f9", help="ID of the Azure tenant. Default is the Turing tenant.")
     args = parser.parse_args()
 
     # Load configuration from file
@@ -154,13 +167,13 @@ def main():
         config = json.load(f_config)
 
     # Get credentials using our tenant ID
-    credentials = authenticate_device_code(tenant="4395f4a7-e455-4f95-8a9f-1fbaef6384f9")
-    subscription_name = config["dsg"]["subscriptionName"]
+    credentials = authenticate_device_code(tenant=args.tenant)
     location = config["dsg"]["location"]
     storage_container_name = "terraformbackend"
 
-    # Switch to the correct subscription
-    subscription, _ = get_subscription(subscription_name, credentials)
+    # Get subscription details
+    subscription, _ = get_subscription(config["dsg"]["subscriptionName"], credentials)
+    management_subscription, _ = get_subscription(config["shm"]["subscriptionName"], credentials)
 
     # Ensure that the resource group exists
     ensure_resource_group(credentials, subscription.subscription_id, args.resource_group, location)
@@ -175,10 +188,10 @@ def main():
 
     # Write configuration to Terraform
     write_terraform_config(storage_account_name, storage_container_name, storage_account_key,
-                           subscription.subscription_id)
+                           subscription.subscription_id, management_subscription.subscription_id)
 
     # Convert config into Terraform variables
-    write_terraform_variables(config, subscription.tenant_id)
+    write_terraform_variables(config, subscription.tenant_id, management_subscription.subscription_id)
 
 
 if __name__ == "__main__":
