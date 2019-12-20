a52cfdac9564117be128680e5482cfb260c463c0
warwick26_wood <wwood@turing.ac.uk>
2019-12-20 15:42:34 +0000

    Placed the disk resizing into the shut down script for vms

--- new_dsg_environment/dsg_deploy_scripts/Shutdown_DSG_VMs.ps1
+++ new_dsg_environment/dsg_deploy_scripts/Shutdown_DSG_VMs.ps1
@@ -1,6 +1,8 @@
 param(
   [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g enter '9' for DSG9)")]
-  [string]$dsgId
+  [string]$dsgId,
+  [Parameter(Position=1, HelpMessage = "Enter Disk SKU to use (defaults to 'Standard_LRS, StandardSSD_LRS,Premium_LRS')")]
+  [string]$storageType = (Read-Host -prompt "Enter VM size to use (defaults to 'Standard_LRS, StandardSSD_LRS,Premium_LRS')")
 )
 
 Import-Module Az
@@ -13,20 +15,34 @@ $config = Get-DsgConfig($dsgId)
 $prevContext = Get-AzContext
 $_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
 
-Write-Host "===Stopping all compute VMs==="
+Write-Host "===Stopping all compute VMs===" -ForegroundColor Cyan
 Get-AzVM -ResourceGroupName $config.dsg.dsvm.rg | Stop-AzVM -Force -NoWait
-Write-Host "===Stopping web app servers==="
+Write-Host "===Stopping web app servers===" -ForegroundColor Cyan
 Stop-AzVM -ResourceGroupName $config.dsg.linux.rg -Name $config.dsg.linux.gitlab.vmName -Force -NoWait
 Stop-AzVM -ResourceGroupName $config.dsg.linux.rg -Name $config.dsg.linux.hackmd.vmName -Force -NoWait
-Write-Host "===Stopping dataserver==="
+Write-Host "===Stopping dataserver===" -ForegroundColor Cyan
 Stop-AzVM -ResourceGroupName $config.dsg.dataserver.rg -Name $config.dsg.dataserver.vmName -Force -NoWait
-Write-Host "===Stopping RDS session hosts==="
+Write-Host "===Stopping RDS session hosts===" -ForegroundColor Cyan
 Stop-AzVM -ResourceGroupName $config.dsg.rds.rg -Name $config.dsg.rds.sessionHost1.vmName -Force -NoWait
 Stop-AzVM -ResourceGroupName $config.dsg.rds.rg -Name $config.dsg.rds.sessionHost2.vmName -Force -NoWait
-Write-Host "===Stopping RDS gateway==="
+Write-Host "===Stopping RDS gateway===" -ForegroundColor Cyan
 Stop-AzVM -ResourceGroupName $config.dsg.rds.rg -Name $config.dsg.rds.gateway.vmName -Force -NoWait
-Write-Host "===Stopping AD DC==="
+Write-Host "===Stopping AD DC===" -ForegroundColor Cyan
 Stop-AzVM -ResourceGroupName $config.dsg.dc.rg -Name $config.dsg.dc.vmName -Force -NoWait
+
+#Pausing the script to allow VM to shutdown.
+Write-Host "Script paused for 200 seconds to allow Servers to Shutdown....." -ForegroundColor Cyan
+Start-Sleep -Seconds 200
+
+# Find and Update the storage type
+
+foreach($disk in Get-AzDisk) { 
+  Write-Host "===Resizing $(($disk).Name) ===" -ForegroundColor Cyan
+$diskUpdateConfig = New-AzDiskUpdateConfig -AccountType $storageType -DiskSizeGB $disk.DiskSizeGB
+Update-AzDisk -DiskUpdate $diskUpdateConfig -ResourceGroupName $disk.ResourceGroupName `
+-DiskName $disk.Name
+}
+
 
 # Switch back to original subscription
 $_ = Set-AzContext -Context $prevContext;
